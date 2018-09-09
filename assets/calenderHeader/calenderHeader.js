036ff9913ef6f8a5de453ac357ffff5e45fac069
codingcolin <c_jax31@yahoo.com>
2018-09-09 09:16:25 -0700

    finished calender header

---
+++ assets/calenderHeader/calenderHeader.js
@@ -0,0 +1,11 @@
+let calenderHeader = () => {
+	let month = document.getElementById('month');
+	let monthSetter = (monthName) => {
+		month.innerHTML = monthName;
+		monthSetter("October");
+	monthName = ["January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	monthSetter.addEventListener('click', ()=>{
+			
+		}
+	};
+}
